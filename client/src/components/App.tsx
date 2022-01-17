b1284299a38c1566426da9c82ce6823be2bbf61d
ThiagoCComelli <thiago.comelli@outlook.com>
2022-01-17 19:25:49 -0300

    Custom Cursor
    shadow cursor delayed movement

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import './App.css';
+import { useEffect, useRef } from 'react';
 import { Navbar } from './Navbar/Navbar';
+import { Login } from '../pages/Login/Login';
+import './App.css';
 
 function App() {
+  
+  useEffect(() => {
+    const cursorRef = document.getElementsByClassName("cursor")[0]
+    document.addEventListener("mousemove", (event) => {
+      event.stopPropagation()
+      cursorRef.setAttribute("style", `top: ${event.pageY - 10}px; left: ${event.pageX - 10}px;`)
+    })  
+    console.log("event")
+    
+  }, [])
+
   return (
     <div className="mainApp">
+      <div className="cursor"></div>
       <Router>
         <Navbar />
         <Routes>
